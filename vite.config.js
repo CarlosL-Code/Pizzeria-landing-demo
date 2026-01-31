ffc15a1280b4eedf0faa12d2a846cfeb17ec5bd2
Carlos Lozano <carloslozanosilva6@gmail.com>
2026-01-31 00:48:09 -0300

    Actualización zona de menu v10

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
+import { resolve } from 'path'; // Importante para las rutas
 
 export default defineConfig({
   base: "./",
   plugins: [
     tailwindcss(),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        menu: resolve(__dirname, 'menu.html'),
+      },
+    },
+  },
 });
