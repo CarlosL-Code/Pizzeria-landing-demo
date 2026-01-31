3359eee0d0765c64fd629496ff974036e6563d28
Carlos Lozano <carloslozanosilva6@gmail.com>
2026-01-30 22:59:48 -0300

    Lading page Restaurant Pizzeria v2

---
+++ src/main.js
@@ -0,0 +1,8 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const page = document.getElementById("page");
+
+  requestAnimationFrame(() => {
+    page.classList.remove("opacity-0", "translate-y-6");
+    page.classList.add("opacity-100", "translate-y-0");
+  });
+});
